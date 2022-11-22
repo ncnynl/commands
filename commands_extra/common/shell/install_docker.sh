5543547485f258e890a91e7c2d94af6d5bc91da2
ncnynl <ncnynl@gmail.com>
2022-11-22 14:21:37 +0800

    update to 1.2.4 add install docker, install robots.ros.org

---
+++ commands_extra/common/shell/install_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+################################################
+# Function : install_docker_compose.sh                              
+# Platform : ubuntu                                
+# Version  : 1.0                               
+# Date     : 2022-10-29 17:17:58                            
+# Author   : ncnynl                             
+# Contact  : 1043931@qq.com                              
+# URL: https://ncnynl.com                                   
+# Licnese: MIT                                 
+# QQ Qun: 创客智造B群:926779095                                  
+# QQ Qun: 创客智造C群:937347681                               
+# QQ Qun: 创客智造D群:562093920                               
+################################################
+        
+sudo apt isntall curl -y 
+#安装最新版本的docker
+curl -sSL https://get.daocloud.io/docker | sh
+
