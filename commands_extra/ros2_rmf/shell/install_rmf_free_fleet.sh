896b652448d9e18463f955a9afd6bb985c4d8359
ncnynl <104391@qq.com>
2022-12-02 14:32:05 +0800

    fix rmf-web script

--- commands_extra/ros2_rmf/shell/install_rmf_free_fleet.sh
+++ commands_extra/ros2_rmf/shell/install_rmf_free_fleet.sh
@@ -12,22 +12,44 @@
 # QQ Qun: 创客智造C群:937347681                               
 # QQ Qun: 创客智造D群:562093920                               
 ################################################
-echo "Not Yet Supported!"
-exit 0        
-#基于ubuntu20.04
+# https://github.com/open-rmf/free_fleet
+# echo "Not Yet Supported!"
+# exit 0        
+#基于ubuntu22.04
 #安装依赖 
 
+if [ -d ~/ros2_free_fleet_ws ]; then 
+  echo "free_fleet have installed!"
+  exit 0
+fi
+
+
 sudo apt update && sudo apt install \
-  git wget qt5-default \
+  git wget  \
   python3-rosdep \
   python3-vcstool \
   python3-colcon-common-extensions \
   # maven default-jdk   # Uncomment to install dependencies for message generation
 
+#install qt5
+# sudo apt install  -y qt5-default
+
+#cyclonedds-tools dds_idlc
+# sudo apt install cyclonedds-tools
+
 
 #downlaod
-mkdir -p ~/ros2_rmf_free_fleet_ws/src
-cd ~/ros2_rmf_free_fleet_ws/src
+mkdir -p ~/ros2_free_fleet_ws/src
+cd ~/ros2_free_fleet_ws/src
 #commit 4f2c897b206d55905f51222499cde5dd36a4c093
-git clone https://ghproxy.com/https://github.com/open-rmf/free_fleet
-cd free_fleet
+git clone -b main https://ghproxy.com/https://github.com/open-rmf/free_fleet
+#have installed in rmf
+# git clone -b main https://ghproxy.com/https://github.com/open-rmf/rmf_internal_msgs 
+
+#rosdep
+cd ~/ros2_free_fleet_ws/
+rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y
+
+#build for ros2
+colcon build --packages-up-to \
+  free_fleet ff_examples_ros2 free_fleet_server_ros2 free_fleet_client_ros2
