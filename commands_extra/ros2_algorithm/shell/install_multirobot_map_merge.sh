cd3bd1773c163879d8c7774bc87f4275fa532ae3
ncnynl <104391@qq.com>
2022-12-13 10:56:56 +0800

    add tb3 launch scripts

---
+++ commands_extra/ros2_algorithm/shell/install_multirobot_map_merge.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+################################################
+# Function : install_multirobot_map_merge                        
+# Platform : ubuntu                                
+# Version  : 1.0                               
+# Date     : 2022-12-10                          
+# Author   : ncnynl                             
+# Contact  : 1043931@qq.com                              
+# URL: https://ncnynl.com                                   
+# Licnese: MIT                
+# Company  :Foshan AiZheTeng Information Technology Co.,Ltd.                 
+# QQ Qun: 创客智造B群:926779095                                  
+# QQ Qun: 创客智造C群:937347681                               
+# QQ Qun: 创客智造D群:562093920                               
+################################################
+# echo "Not Supported Yet!"
+# exit 0  
+echo ""
+echo "Set workspace"
+workspace=ros2_multirobot_ws
+
+echo ""
+echo "Set soft name"
+soft_name=m-explore-ros2
+
+echo ""
+echo "Workspace if exits ?"
+if [ ! -d ~/$workspace ];then 
+    mkdir -p ~/$workspace/src
+fi 
+
+echo ""
+echo "Software if installed ?"
+if [ -d ~/$workspace/src/$soft_name ];then 
+    echo "$soft_name have installed" && exit 0
+fi 
+
+echo ""
+echo "Install system deps"
+
+# 下载源码
+echo ""
+echo "Download source"
+cd ~/$workspace/src
+
+
+if [ ${ROS_DISTRO} == 'humble' ];then
+    git clone -b feature/slam_toolbox_compat https://ghproxy.com/https://github.com/owen7900/m-explore-ros2
+else
+    git clone -b feature/slam_toolbox_compat https://ghproxy.com/https://github.com/robo-friends/m-explore-ros2
+fi
+
+echo ""
+echo "Install rosdeps"
+cd ~/$workspace/
+rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y
+
+
+# 编译代码
+echo "Compile source"
+cd ~/$workspace/
+colcon build --symlink-install --cmake-args "-DBUILD_TESTING=OFF"
+
+
+echo "Add workspace to bashrc if not exits"
+if ! grep -Fq "$workspace/install/local_setup.bash" ~/.bashrc
+then
+    echo ". ~/$workspace/install/local_setup.bash" >> ~/.bashrc
+    echo " $workspace workspace have installed successfully! writed to ~/.bashrc"
+else
+    echo "Has been inited before! Please check ~/.bashrc"
+fi
+
+#How to use
