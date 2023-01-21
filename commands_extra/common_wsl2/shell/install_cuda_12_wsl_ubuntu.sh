5a8b29d690b7336c1eae5fd1033a87c18c76a653
ncnynl <1043931@qq.com>
2023-01-21 18:29:12 +0800

    install cuda 12 wsl ubuntu

---
+++ commands_extra/common_wsl2/shell/install_cuda_12_wsl_ubuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+################################################
+# Function : install cuda 12.0 wsl ubuntu   
+# Desc     : 用于wsl上安装cuda toolkit的脚本 
+# Website  : https://www.ncnynl.com/archives/202301/5826.html                          
+# Platform : WSL2 ubuntu                                
+# Version  : 1.0                               
+# Date     : 2022-01-10                             
+# Author   : ncnynl                             
+# Contact  : 1043931@qq.com                              
+# URL: https://ncnynl.com                                   
+# Licnese: MIT                                 
+# QQ Qun: 创客智造B群:926779095                                  
+# QQ Qun: 创客智造C群:937347681                               
+# QQ Qun: 创客智造D群:562093920                               
+################################################
+
+echo "Install  Cuda 12.0 toolkit ， this will take a long time to download cuda wsl ubuntu ...."
+
+sudo apt-key del 7fa2af80
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+echo "Downloading cuda-repo-wsl-ubuntu-12-0-local_12.0.0-1_amd64.deb"
+# wget https://developer.download.nvidia.cn/compute/cuda/12.0.0/local_installers/cuda-repo-wsl-ubuntu-12-0-local_12.0.0-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda-repo-wsl-ubuntu-12-0-local_12.0.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-12-0-local_12.0.0-1_amd64.deb
+
+echo "Install cuda"
+sudo cp /var/cuda-repo-wsl-ubuntu-12-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+
+echo "Config bashrc"
+echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PAT' >> ~/.bashrc
+
