a2db21feb2e48403587b6c1a06b04e9e25942293
ncnynl <1043931@qq.com>
2023-01-21 22:17:21 +0800

    no root use docker

--- commands_extra/common/shell/install_docker.sh
+++ commands_extra/common/shell/install_docker.sh
@@ -14,7 +14,21 @@
 # QQ Qun: 创客智造D群:562093920                               
 ################################################
         
+echo "Install Curl"
 sudo apt isntall curl -y 
-#安装最新版本的docker
+
+echo "安装最新版本的docker"
 curl -sSL https://get.daocloud.io/docker | sh
+
+echo "无root 权限能否使用 docker"
+sudo groupadd docker			# 有则不用创建
+sudo usermod -aG docker $USER	# USER 为加入 docker 组的用户
+newgrp docker					# 刷新 docker 组    
+
+echo "Check Docker version"
+docker --version 
+
+# docker run hello-world			# 测试无 root 权限能否使用 docker
+
+
 
