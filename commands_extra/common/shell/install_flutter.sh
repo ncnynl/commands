27616cd992e3b12ecd3406b6337bacf6037f527a
ncnynl <1043931@qq.com>
2023-02-01 18:08:28 +0800

    flutter install

--- commands_extra/common/shell/install_flutter.sh
+++ commands_extra/common/shell/install_flutter.sh
@@ -12,19 +12,25 @@
 # QQ Qun: 创客智造C群:937347681                                  
 # QQ Qun: 创客智造D群:562093920                               
 ################################################
-
+# https://www.cyberithub.com/how-to-install-flutter-on-ubuntu-20-04-lts-focal-fossa/
+# https://docs.flutter.dev/community/china
+# https://flutter.cn/docs/get-started/install/linux
 
 cd $HOME/tools
 
 git clone https://ghproxy.com/https://github.com/flutter/flutter.git -b stable
 
 echo "add path to bashrc"
-
 echo 'export PATH=$PATH:$HOME/tools/flutter/bin' >> ~/.bashrc 
+echo 'export PUB_HOSTED_URL=https://pub.flutter-io.cn' >> ~/.bashrc 
+echo 'export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn' >> ~/.bashrc 
+echo 'export FLUTTER_GIT_URL=https://ghproxy.com/https://github.com/flutter/flutter.git' >> ~/.bashrc 
 
-echo "
-  $ which flutter dart
-  /path-to-flutter-sdk/bin/flutter
-  /path-to-flutter-sdk/bin/dart
-"
+export PATH=$PATH:$HOME/tools/flutter/bin
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+export FLUTTER_GIT_URL=https://ghproxy.com/https://github.com/flutter/flutter.git
+
+which flutter dart
+
 echo "Congratulations, flutter have successfully installed"
