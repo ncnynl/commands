594177866e386eca8d957589de86236cfd7246a0
ncnynl <1043931@qq.com>
2023-03-14 12:10:30 +0800

    add orb slamv2

---
+++ commands_extra/common/shell/install_pangolin.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+################################################
+# Function : Install Pangolin source version
+# Desc     : 用于源码方式安装Pangolin的脚本                       
+# Platform : ubuntu                                
+# Version  : 1.0                               
+# Date     : 2022-12-10                          
+# Author   : ncnynl                             
+# Contact  : 1043931@qq.com                              
+# URL: https://ncnynl.com                                   
+# Licnese: MIT                                 
+# QQ Qun: 创客智造B群:926779095                                  
+# QQ Qun: 创客智造C群:937347681                               
+# QQ Qun: 创客智造D群:562093920                               
+################################################
+export TEXTDOMAINDIR=/usr/share/locale
+export TEXTDOMAIN=commands        
+echo "$(gettext "Install Pangolin source version")"
+# 
+echo "Not Supported Yet!"
+exit 0  
+echo ""
+echo "Set workspace"
+workspace=tools
+
+echo ""
+echo "Set soft name"
+soft_name=Pangolin
+
+echo ""
+echo "Workspace if exits ?"
+if [ ! -d ~/$workspace ];then 
+    mkdir -p ~/$workspace
+fi 
+
+echo ""
+echo "Software if installed ?"
+if [ -d ~/$workspace/Pangolin ];then 
+    echo "$soft_name have installed" && exit 0
+fi 
+
+echo ""
+echo "Install system deps"
+
+# 下载源码
+echo ""
+echo "Download source"
+cd ~/$workspace
+git clone https://ghproxy.com/https://github.com/stevenlovegrove/Pangolin
+
+# 编译代码
+echo "Compile source"
+cd Pangolin
+mkdir build
+cd build
+cmake -DCPP11_NO_BOOST=1 ..
+make
+
+#How to use
--- commands_extra/ros2_algorithm/shell/install_ros2_orb_slamv2_ws.sh
+++ commands_extra/ros2_algorithm/shell/install_ros2_orb_slamv2_ws.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 ################################################
-# Function : Install ros2 lio_sam gazebo shell  
-# Desc     : 源码安装lio_sam并实现多线雷达仿真建图脚本                      
+# Function : Install ROS2 ORB_SLAMV2 source version
+# Desc     : 用于源码方式安装ROS2版ORB_SLAMV2的脚本                       
 # Platform : ubuntu                                
 # Version  : 1.0                               
-# Date     : 2023-03-10                          
+# Date     : 2022-12-10                          
 # Author   : ncnynl                             
 # Contact  : 1043931@qq.com                              
 # URL: https://ncnynl.com                                   
-# Licnese: MIT                
-# Company  :Foshan AiZheTeng Information Technology Co.,Ltd.                 
+# Licnese: MIT                                 
 # QQ Qun: 创客智造B群:926779095                                  
 # QQ Qun: 创客智造C群:937347681                               
 # QQ Qun: 创客智造D群:562093920                               
 ################################################
-# https://github.com/TixiaoShan/LIO-SAM/tree/ros2
-
 export TEXTDOMAINDIR=/usr/share/locale
 export TEXTDOMAIN=commands        
-echo "$(gettext "Install ros2 lio_sam gazebo shell")"
-# echo "Not Supported Yet!"
-# exit 0  
+echo "$(gettext "Install ROS2 ORB_SLAMV2 source version")"
+
 # for humble
 cs -rv "humble"
 if [ 1 == $? ];then 
     exit 0
 fi
 
+# echo "Not Supported Yet!"
+# exit 0  
 echo ""
 echo "Set workspace"
-workspace=ros2_lio_sam_ws
+workspace=ros2_orb_slamv2_ws
 
 echo ""
 echo "Set soft name"
-soft_name=robot_gazebo
+soft_name=ros2-ORB_SLAM2
+
+echo ""
+echo "Workspace if exits ?"
+if [ ! -d ~/$workspace ];then 
+    mkdir -p ~/$workspace/src
+fi 
 
 echo ""
 echo "Software if installed ?"
@@ -41,57 +45,34 @@ if [ -d ~/$workspace/src/$soft_name ];then
     echo "$soft_name have installed" && exit 0
 fi 
 
-# sudo snap install cloudcompare 
-# cs -si install cloudcompare
-
-#install gtsam
-# https://gtsam.org/get_started/
-# cs -si install_gtsam_source.sh
-
 echo ""
 echo "Install system deps"
-sudo apt install python3-colcon-common-extensions -y
-sudo apt install ros-humble-gazebo-ros-pkgs -y
-sudo apt install ros-humble-gazebo-ros -y
-sudo apt install ros-humble-gazebo-ros -y
-sudo apt install ros-humble-ros2-control -y
-sudo apt install ros-humble-ros2-controllers -y
-sudo apt install ros-humble-perception-pcl \
-  	   ros-humble-pcl-msgs \
-  	   ros-humble-vision-opencv \
-  	   ros-humble-xacro
 
 # 下载源码
 echo ""
 echo "Download source"
-cd ~
-git clone  http://gitee.com/ncnynl/ros2_lio_sam_ws
+cd ~/$workspace/src
+
+echo "Choose branch is ${branch}"
+git clone  https://github.com/EndlessLoops/ros2-ORB_SLAM2
 
 echo ""
 echo "Install rosdeps"
 cd ~/$workspace/
 rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y
 
-
 # 编译代码
 echo "Compile source"
 cd ~/$workspace/
 colcon build --symlink-install 
 
-
 echo "Add workspace to bashrc if not exits"
 if ! grep -Fq "$workspace/install/local_setup.bash" ~/.bashrc
 then
     echo ". ~/$workspace/install/local_setup.bash" >> ~/.bashrc
-    echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/ros2_lio_sam_ws/src/robot_gazebo/models/" >> ~/.bashrc
     echo " $workspace workspace have installed successfully! writed to ~/.bashrc"
 else
     echo "Has been inited before! Please check ~/.bashrc"
 fi
+
 #How to use
-# ros2 launch robot_gazebo robot_sim.launch.py
-# ros2 launch lio_sam run.launch.py
-# ros2 run teleop_twist_keyboard teleop_twist_keyboard
-# /home/ubuntu/Dowloads/LOAM
-# cloudcompare open cloudGlobal.pcd file
-# ros2 run rqt_tf_tree rqt_tf_tree
