e09525f0f610dd26f5d4f3571abdf525fdb08678
ncnynl <1043931@qq.com>
2023-04-13 12:28:29 +0800

    add gmapping

---
+++ commands_extra/ros2_algorithm/shell/install_ros2_gmapping_source.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+################################################################
+# Function :Install ROS2 gmapping source version           #
+# Desc     : 用于源码方式安装ROS2版激光建图算法gmapping的脚本   
+# Platform :All Linux Based Platform                           #
+# Version  :1.0                                                #
+# Date     :2022-06-23                                         #
+# Author   :ncnynl                                             #
+# Contact  :1043931@qq.com                                     #
+# Company  :Foshan AiZheTeng Information Technology Co.,Ltd.   #
+# URL: https://ncnynl.com                                      #
+################################################################
+export TEXTDOMAINDIR=/usr/share/locale
+export TEXTDOMAIN=commands        
+echo "$(gettext "Install ROS2 gmapping source version")"
+
+workspace=ros2_algorithm_ws
+
+if [ -d ~/$workspace/src/slam_gmapping ]; then
+    echo "slam_gmapping have installed!!" 
+else
+
+    #install deps
+    sudo apt-get update
+
+    echo "Download ROS2 ${ROS_DISTRO} gmapping"
+    if [ ! -d ~/$workspace/src ];then
+        mkdir -p ~/$workspace/src
+    fi
+
+    cd ~/$workspace/src
+    # git clone https://github.com/Project-MANAS/slam_gmapping
+    # https://github.com/charlielito/slam_gmapping/tree/feature/namespace_launch
+    git clone https://ghproxy.com/https://github.com/charlielito/slam_gmapping.git --branch feature/namespace_launch
+
+    rosdep update
+    rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
+
+    #Build and install
+    colcon build --symlink-install --packages-select openslam_gmapping slam_gmapping
+
+    #add to bashrc if not exits
+    echo "Please add workspace to bashrc 'cs -ss load_ros2_algorithm_ws'"
+fi
