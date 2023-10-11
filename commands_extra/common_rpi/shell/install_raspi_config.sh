64f4f76ebb429df9730fbaf0b0c902e3c9083065
ncnynl <1043931@qq.com>
2023-10-11 08:32:51 +0800

    增加树莓派开发板相关的目录和脚本

--- commands_extra/common_rpi/shell/install_raspi_config.sh
+++ commands_extra/common_rpi/shell/install_raspi_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ################################################################
-# Function : install_bot_box 
-# Desc     : 安装roboportal的远程控制客户端bot_box的脚本                         
+# Function : install_raspi_config 
+# Desc     : 安装摄像头驱动的脚本                         
 # Platform : ubuntu                                
 # Version  : 1.0                               
-# Date     : Tue Oct 10 01:43:22 PM CST 2023                            
+# Date     : Wed Oct 11 08:24:04 AM CST 2023                            
 # Author   : ncnynl                             
 # Contact  : 1043931@qq.com     
 # Company  : Foshan AiZheTeng Information Technology Co.,Ltd.                            
@@ -14,30 +14,23 @@
 ################################################################
 export TEXTDOMAINDIR=/usr/share/locale
 export TEXTDOMAIN=commands        
-echo "$(gettext "install_bot_box")"
+echo "$(gettext "install_raspi_config")"
 
 source ${HOME}/commands/cs_utils_ros.sh
 
 echo "This script is under DEV state !"
 
-echo " From https://github.com/roboportal/bot_box"
-
 function _rcm_run_() {
 
-    package_name="bot_box"
-
-    echo "Please finish code first!"
-    return 
     # if installed ?
-    if [ -d ~/tools/$package_name ]; then
+    raspi=$(which is raspi-config)
+    if [ $raspi ]; then
         echo "$package_name have installed!!" 
     else 
 
         echo "Install related system dependencies"
         sudo apt-get update
-        sudo apt install -y git wget libatomic-ops-dev pkg-config libvpx-dev libopus-dev libopusfile-dev libasound2-dev libsodium-dev libzmq3-dev libczmq-dev
-        sudo apt install -y supervisor
-        sudo apt install -y build-essential
+        sudo apt install lua5.1 libatopology2 libfftw3-single3 libsamplerate0 alsa-utils
 
         echo "Go to workspace"
         if [ ! -d ~/tools ]; then
@@ -49,31 +42,23 @@ function _rcm_run_() {
         #run import
         echo "this will take a while to download"
         echo "Dowload $package_name"
+        wget http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20201108_all.deb
 
-        # install go
-        echo " if to slow ,see: https://cyfeng.science/2020/06/15/speed-up-ubuntu-snap/, need to proxy!"
-        sudo snap install go --classic
+        echo "Build the code"
+        sudo dpkg -i raspi-config_20201108_all.deb
+        sudo usermod -aG video ubuntu
 
-        # install bot_box
-        echo "Configure git proxy"
-        git config --global url."https://ghproxy.com/https://github.com".insteadof https://github.com    
-
-        git clone https://github.com/roboportal/bot_box.git
-        cd ./bot_box
-        go build
-        # CGO_LDFLAGS="-latomic" go build -tags=arm64
-        # CGO_LDFLAGS="-latomic" go build -tags=arm
-
+        echo "Finish configuration , Please check URL : https://www.ncnynl.com/archives/202110/4813.html "
     fi
 }
 
 function _rcm_usage_() {
     cat << EOF
 Usage:
-    install_bot_box 
+    install_raspi_config 
 
 Description:
-    安装roboportal的远程控制客户端bot_box的脚本
+    安装摄像头驱动的脚本
 
 Option:
     --help|-h:                                         -- using help
@@ -129,7 +114,7 @@ function rcm_execute() {
     fi
 
     # start
-    echo "install_bot_box start ..."
+    echo "install_raspi_config start ..."
     _rcm_run_ $*
 
     if [[ $debug == 1 ]]; then
