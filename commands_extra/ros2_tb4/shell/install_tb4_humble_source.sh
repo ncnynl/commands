a8dee67fa32f41113f830471f08c2da332a8971d
ncnynl <1043931@qq.com>
2024-07-19 16:01:15 +0800

    fix: install tb4

--- commands_extra/ros2_tb4/shell/install_tb4_humble_source.sh
+++ commands_extra/ros2_tb4/shell/install_tb4_humble_source.sh
@@ -46,11 +46,6 @@ else
     # 获取仓库列表
 
     git clone -b humble https://github.com/turtlebot/turtlebot4_simulator.git
-    #fix ignition-gui5 to ignition-gui6
-    if [ ${ROS_DISTRO} == "humble" ]; then
-        sed -i 's/ignition-gui5/ignition-gui6/'g ~/ros2_tb4_ws/src/turtlebot4_simulator/turtlebot4_ignition_gui_plugins/package.xml
-        sed -i 's/ignition-gui5/ignition-gui6/'g ~/ros2_tb4_ws/src/turtlebot4_simulator/turtlebot4_ignition_gui_plugins/Turtlebot4Hmi/CMakeLists.txt
-    fi
 
     #run import
     echo "this will take 10-30 min to download"
@@ -61,66 +56,7 @@ else
     git clone -b ${ROS_DISTRO} https://github.com/iRobotEducation/irobot_create_msgs.git
 
     echo "Dowload from create3_sim support namespace"
-    # git clone -b humble https://github.com/iRobotEducation/create3_sim.git
-    #commit e589410aa4ee483c4a3948c55a6b53c37118143a
-    git clone -b asoragna/${ROS_DISTRO} https://github.com/iRobotEducation/create3_sim.git
-    # git clone -b feat/create3_namespace https://github.com/sp-sophia-labs/create3_sim.git
-    #fix ignition-gui5 to ignition-gui6
-    if [ ${ROS_DISTRO} == "humble" ]; then
-        sed -i 's/ignition-gui5/ignition-gui6/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_plugins/package.xml
-        sed -i 's/ignition-gui5/ignition-gui6/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_plugins/CMakeLists.txt
-    fi
-    # fix msg/dock.hpp to msg/dock_status.hpp
-    #fix msg::Dock to msg::DockStatus
-    # ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/robot_state.hpp
-    # ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/robot_state.cpp
-    # ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/src/sensors/ir_opcode.cpp
-    # ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/include/irobot_create_ignition_toolbox/sensors/ir_opcode.hpp
-    # ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/include/irobot_create_gazebo_plugins/gazebo_ros_docking_status.hpp
-    # ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/src/gazebo_ros_docking_status.cpp
-
-    # fix action/dock_servo.hpp to action/dock.hpp
-    # fix action::DockServo to action::Dock 
-    # ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/motion_control/docking_behavior.hpp
-    # ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/motion_control/docking_behavior.cpp
-
-    if [ ${ROS_DISTRO} == "humble" ]; then
-        sed -i 's/msg\/dock.hpp/msg\/dock_status.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/robot_state.hpp
-        sed -i 's/msg\/dock.hpp/msg\/dock_status.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/robot_state.cpp
-        sed -i 's/msg\/dock.hpp/msg\/dock_status.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/src/sensors/ir_opcode.cpp
-        sed -i 's/msg\/dock.hpp/msg\/dock_status.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/include/irobot_create_ignition_toolbox/sensors/ir_opcode.hpp
-        sed -i 's/msg\/dock.hpp/msg\/dock_status.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/include/irobot_create_gazebo_plugins/gazebo_ros_docking_status.hpp
-        sed -i 's/msg\/dock.hpp/msg\/dock_status.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/src/gazebo_ros_docking_status.cpp
-        sed -i 's/msg\/dock.hpp/msg\/dock_status.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/motion_control/docking_behavior.hpp
-        sed -i 's/msg\/dock.hpp/msg\/dock_status.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/motion_control/docking_behavior.cpp
-        
-        sed -i 's/msg::Dock/msg::DockStatus/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/robot_state.hpp
-        sed -i 's/msg::Dock/msg::DockStatus/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/robot_state.cpp
-        sed -i 's/msg::Dock/msg::DockStatus/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/src/sensors/ir_opcode.cpp
-        sed -i 's/msg::Dock/msg::DockStatus/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/include/irobot_create_ignition_toolbox/sensors/ir_opcode.hpp
-        sed -i 's/msg::Dock/msg::DockStatus/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/include/irobot_create_gazebo_plugins/gazebo_ros_docking_status.hpp
-        sed -i 's/msg::Dock/msg::DockStatus/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/src/gazebo_ros_docking_status.cpp
-        sed -i 's/msg::Dock/msg::DockStatus/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/motion_control/docking_behavior.hpp
-        sed -i 's/msg::Dock/msg::DockStatus/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/motion_control/docking_behavior.cpp
-
-        sed -i 's/action\/dock_servo.hpp/action\/dock.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/robot_state.hpp
-        sed -i 's/action\/dock_servo.hpp/action\/dock.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/robot_state.cpp
-        sed -i 's/action\/dock_servo.hpp/action\/dock.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/src/sensors/ir_opcode.cpp
-        sed -i 's/action\/dock_servo.hpp/action\/dock.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/include/irobot_create_ignition_toolbox/sensors/ir_opcode.hpp
-        sed -i 's/action\/dock_servo.hpp/action\/dock.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/include/irobot_create_gazebo_plugins/gazebo_ros_docking_status.hpp
-        sed -i 's/action\/dock_servo.hpp/action\/dock.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/src/gazebo_ros_docking_status.cpp
-        sed -i 's/action\/dock_servo.hpp/action\/dock.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/motion_control/docking_behavior.hpp
-        sed -i 's/action\/dock_servo.hpp/action\/dock.hpp/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/motion_control/docking_behavior.cpp
-
-        sed -i 's/action::DockServo/action::Dock/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/robot_state.hpp
-        sed -i 's/action::DockServo/action::Dock/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/robot_state.cpp
-        sed -i 's/action::DockServo/action::Dock/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/src/sensors/ir_opcode.cpp
-        sed -i 's/action::DockServo/action::Dock/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_ignition/irobot_create_ignition_toolbox/include/irobot_create_ignition_toolbox/sensors/ir_opcode.hpp
-        sed -i 's/action::DockServo/action::Dock/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/include/irobot_create_gazebo_plugins/gazebo_ros_docking_status.hpp
-        sed -i 's/action::DockServo/action::Dock/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_gazebo/irobot_create_gazebo_plugins/src/gazebo_ros_docking_status.cpp
-        sed -i 's/action::DockServo/action::Dock/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/include/irobot_create_nodes/motion_control/docking_behavior.hpp
-        sed -i 's/action::DockServo/action::Dock/'g ~/ros2_tb4_ws/src/create3_sim/irobot_create_common/irobot_create_nodes/src/motion_control/docking_behavior.cpp
-    fi
+    git clone -b humble https://github.com/iRobotEducation/create3_sim.git
 
     echo "Dowload from turtlebot4 common "
     git clone -b ${ROS_DISTRO} https://github.com/turtlebot/turtlebot4.git
@@ -144,10 +80,13 @@ else
     # colcon build  --mixin release lld
     colcon build --symlink-install  --parallel-workers 1  
 
-    #run echo
-    echo "Install turtlebot4 source successfully"
-
-    echo "Load workspace execute : cs -ss load_tb4 -add "
-    # cs -ss load_tb4 -add 
+    echo "Add workspace to bashrc if not exits"
+    if ! grep -Fq "$workspace/install/local_setup.bash" ~/.bashrc
+    then
+        echo ". ~/$workspace/install/local_setup.bash" >> ~/.bashrc
+        echo " $workspace workspace have installed successfully! writed to ~/.bashrc"
+    else
+        echo "Has been inited before! Please check ~/.bashrc"
+    fi
 
 fi
